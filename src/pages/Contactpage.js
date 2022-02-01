9e50a6667e073e28cb0e93fdcd2ac464d667b197
LJKR2020 <loredana.rietveld@gmail.com>
2022-02-01 16:20:05 +0100

    added 4 tiles with no content

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Tile from './../components/Tile'
+import "./homepage.css"
 
 function Contact() {
     return (
-        <p>
-            Ik ben de contactpagina
-        </p>
+        <>
+            <article>
+                <Tile
+                    title="Contact"
+                    p1="Deze webapplicatie is gemaakt door Loredana Kraamer-Rietveld"
+                    p3="Heb je vragen of feedback? Stuur dan een e-mail naar: feedback@outlook.com"
+                />
+                <Tile
+                    image="{}"
+                    text="nog even niks"
+                />
+            </article>
+            <article>
+                <Tile
+                    image="{}"
+                    text="nog even niks"
+                />
+                {/*laatste tegel google maps van duurzame locatie: toekomst feature*/}
+                <Tile
+                    image="{}"
+                    text="nog even niks"
+                />
+            </article>
+            
+        </>
     )
 }
 
