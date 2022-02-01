a6a4aaa0badb1858f0ad0169599ccca9a965d517
LJKR2020 <loredana.rietveld@gmail.com>
2022-02-01 11:28:27 +0100

    added svg pictures and jpg for trial and 4 article tags in Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 function Homepage() {
     return (
-        <p>
-            Ik ben de homepagina
-        </p>
+        <>
+            <article>
+                <h1>
+                    Ik ben de homepagina
+                </h1>
+            </article>
+            <article>
+                <img src="./../assets/pexels-karolina-grabowska-6659559.svg" alt="kaas raspen boven een kom spaghetti"/>
+            </article>
+            <article>
+                <img src="./../assets/pexels-karolina-grabowska-6659559.jpg" alt="kaas raspen boven een kom spaghetti"/>
+            </article>
+            <article>
+
+            </article>
+
+        </>
+
     )
 }
 
