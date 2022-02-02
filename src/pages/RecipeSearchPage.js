d79ca9959995213dea054882114555ef6ad0a2a3
LJKR2020 <loredana.rietveld@gmail.com>
2022-02-02 13:55:06 +0100

    buttons in a div and added space between both buttons

--- src/pages/RecipeSearchPage.js
+++ src/pages/RecipeSearchPage.js
@@ -7,17 +7,20 @@ function RecipeSearch() {
            <div className="basic-form">
                <h1>Recepten Generator</h1>
                <form action="/">
-                   <label htmlFor="ingredient">
-                       <input type="text" id="ingredient" placeholder="Ingrediënten"/>
-                   </label>
-                   <label htmlFor="recipe-name">
-                       <input type="text" id="recipe-name" placeholder="Naam van een recept"/>
-                   </label>
-                   <label htmlFor="recipe-name">
-                       <input type="text" id="recipe-name" placeholder="Naam van een recept"/>
-                   </label>
-                   {/*<button type="button">willekeurig recept</button>*/}
-                   <button type="button">Zoeken</button>
+                       <label htmlFor="ingredient">
+                           <input type="text" id="ingredient" placeholder="Ingrediënten"/>
+                       </label>
+                       <label htmlFor="recipe-name">
+                           <input type="text" id="recipe-name" placeholder="Naam van een recept"/>
+                       </label>
+                       <label htmlFor="recipe-name">
+                           <input type="text" id="" placeholder=""/>
+                       </label>
+                   <div>
+                       <button className="buttons" type="button">willekeurig recept</button>
+                       <button className="buttons" type="button">Zoeken</button>
+                   </div>
+
                </form>
            </div>
        </>
