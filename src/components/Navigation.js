a10a220794ede7b5b492f14dc832f8d7290847e0
LJKR2020 <loredana.rietveld@gmail.com>
2022-07-09 11:08:40 +0200

    added logo and styling of logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,22 @@
 import React, {useContext} from 'react';
-import { NavLink} from "react-router-dom";
+import { NavLink} from 'react-router-dom';
 import AuthContext from '../AuthContext';
-import './navigation.css'
+import './navigation.css';
+import logo from './../assets/Bestek.png'
 
 function Navigation() {
     const isAuth = useContext(AuthContext);
 
     return <nav>
-        <img src="/" alt="logo"/>
+        <img src={logo} alt='logo' className='logo'/>
         <ul>
-            <li className="navbar"><NavLink exact to="/">Home</NavLink></li>
-            { isAuth && <li className="navbar"><NavLink to="/recepten">Recipes</NavLink></li> }
-            <li className="navbar"><NavLink to="/contact">Contact</NavLink></li>
-            { !isAuth && <li className="navbar"><NavLink to="/registreren">Sign-Up</NavLink></li> }
-            { !isAuth && <li className="navbar"><NavLink to="/inloggen">Sign-In</NavLink></li> }
-            {/*{ isAuth && <li className="navbar"><NavLink to="/profiel">Profile</NavLink></li> }*/}
-            {/*{ isAuth && <li className="navbar">Log-Out</li>}*/}
+            <li className='navbar'><NavLink exact to='/'>Home</NavLink></li>
+            { isAuth && <li className='navbar'><NavLink to='/recepten'>Recipes</NavLink></li> }
+            <li className='navbar'><NavLink to='/contact'>Contact</NavLink></li>
+            { !isAuth && <li className='navbar'><NavLink to='/registreren'>Sign-Up</NavLink></li> }
+            { !isAuth && <li className='navbar'><NavLink to='/inloggen'>Sign-In</NavLink></li> }
+            { isAuth && <li className='navbar'><NavLink to='/profiel'>Profile</NavLink></li> }
+            { isAuth && <li className='navbar'>Log-Out</li>}
         </ul>
     </nav>
 }
