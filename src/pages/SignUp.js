f1be660fe76f05c7fd79ed31800372069be2911b
LJKR2020 <loredana.rietveld@gmail.com>
2022-07-09 23:15:56 +0200

    Handled errormessage

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -11,9 +11,13 @@ function SignUp() {
     const [password, setPassword] = useState('');
     const [invalidPasswords, setInvalidPasswords] = useState(false);
     // const [isLoading, setIsLoading] = useState(false);
-    // const [hasError, setHasError] = useState(false);
+    const [usernameError, setUsernameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
 
     useEffect(() => {
+        setUsernameError(false);
+        setEmailError(false);
+
         async function signUp() {
             try {
                 const response = await axios.post(`${REACT_APP_AUTH_API_URL}/auth/signup`, {
@@ -23,9 +27,12 @@ function SignUp() {
                 })
                 console.log(response)
             } catch (e) {
-                //errormessage toevoegen
-                //isloading toevoegen
-                console.error(e);
+                if (e.response.data['message'] === 'This username is already in use') {
+                    setUsernameError(true);
+                }
+                if (e.response.data['message'] === 'This email is already in use') {
+                    setEmailError(true);
+                }
             }
         }
 
@@ -58,10 +65,12 @@ function SignUp() {
                 <h1>Sign-up</h1>
                 <form onSubmit={onSubmit}>
                     <label htmlFor='username'>
-                        <input type='text' id='username' placeholder='Username'/>
+                        <input type='text' id='username' placeholder='Username' />
+                        { usernameError && <p className='errormessage'>This username is already in use.</p> }
                     </label>
                     <label htmlFor='email'>
-                        <input type='email' id='email' placeholder='E-mail address'/>
+                        <input type='email' id='email' placeholder='E-mail address' />
+                        { emailError && <p className='errormessage'>This email address is already in use.</p> }
                     </label>
                     <label htmlFor='password'>
                         <input type='password' id='password' placeholder='Password'/>
@@ -69,7 +78,7 @@ function SignUp() {
                     <label htmlFor='password-check'>
                         <input type='password' id='password-check' placeholder='Repeat password'/>
                     </label>
-                    { invalidPasswords && <p className='error'>Passwords aren't the same</p>}
+                    { invalidPasswords && <p className='error'>Passwords aren't the same</p> }
                     <button type='submit'>Sign-up</button>
                 </form>
             </div>
